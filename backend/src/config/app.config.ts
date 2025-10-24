4451bc66c7b173eb554523ef27175f17199a77dd
liaoxiaoyou <liaoxiaoyou@qiniu.com>
2025-10-25 07:32:07 +0800

    feat: update package-lock.json

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 4000,
+  port: parseInt(process.env.PORT!, 10) || 4000,
   ai: {
     openaiApiKey: process.env.OPENAI_API_KEY,
   },
